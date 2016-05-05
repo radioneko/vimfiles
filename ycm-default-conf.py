b441560b14c8fbf35d190cc8db214cb8e3d4ebdc
radioneko <kawaii.neko@pochta.ru>
2016-05-05 12:38:43 +0300

    update ycm-default-conf

--- ycm-default-conf.py
+++ ycm-default-conf.py
@@ -4,11 +4,20 @@ default_cflags = ['-xc', '-std=c99', '-D_GNU_SOURCE', '-I/usr/include/libxml2']
 default_cxxflags = ['-xc++', '-std=c++11', '-D_GNU_SOURCE', '-I/usr/include/libxml2']
 
 def FlagsForFile(filename, **kwargs):
+    try:
+        with open(os.path.dirname(filename) + '.clang_complete') as f:
+            content = f.readlines()
+        def_cflags = ['-xc'] + content
+        def_cxxflags = ['-xc++'] + content
+    except:
+        def_cflags = default_cflags
+        def_cxxflags = default_cxxflags
+
     ext = os.path.splitext(filename)
     if ext in ['.h', '.c']:
-        flags = default_cflags
-    elif ext in ['.cpp', '.cxx', '.cc', '.hpp']:
-        flags = default_cxxflags
+        flags = def_cflags
+    elif ext in ['.cpp', '.cxx', '.cc', '.hpp', '.hh']:
+        flags = def_cxxflags
     return {
         'flags': ['-D_GNU_SOURCE'],
         'do_cache': True
